43ad4bd9e68146c3a159a595634865423a581552
Chinar Shah <chinar.shah@student.unsw.edu.au>
2024-09-13 10:34:39 +0200

    Task 1, 2 and 3 for A1 completed

--- week1/handout/src/APL/AST.hs
+++ week1/handout/src/APL/AST.hs
@@ -1,4 +1,26 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use newtype instead of data" #-}
 module APL.AST
   (
+    Exp(..),
+    VName
   )
 where
+    data Exp
+      = CstInt Integer
+      | Add Exp Exp
+      | Sub Exp Exp
+      | Mul Exp Exp
+      | Div Exp Exp
+      | Pow Exp Exp
+      | CstBool Bool
+      | Eql Exp Exp
+      | If Exp Exp Exp
+      | Var VName
+      | Let VName Exp Exp
+      | Lambda VName Exp 
+      | Apply Exp Exp 
+      | TryCatch Exp Exp
+      deriving (Eq, Show)
+
+    type VName = String
