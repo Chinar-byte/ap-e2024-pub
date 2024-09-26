6259e697eda0a0fce6a5631b2de0a79c28f76946
Chinar Shah <chinar.shah@student.unsw.edu.au>
2024-09-26 20:48:51 +0200

    parser

--- week2/handout/src/APL/Check.hs
+++ week2/handout/src/APL/Check.hs
@@ -8,65 +8,30 @@ where
 
 import APL.AST (Exp (..), VName)
 import Control.Monad (ap, liftM)
+import GHC.IO.Handle.Types (checkHandleInvariants)
 
 type Error = String
 
--- newtype CheckM a = CheckM ([VName] -> (Maybe Error, a))
+-- Monad type
+newtype CheckM a = CheckM (Exp -> Maybe Error)
 
--- instance Functor CheckM where
---   fmap f (CheckM m) = CheckM $ \env ->
---     let (err, x) = m env
---     in (err, f x)
+-- Functor
+instance Functor CheckM where
+  fmap = liftM
 
--- instance Applicative CheckM where
---   pure x = CheckM $ \env -> (Nothing, x)
---   (<*>) = ap
+instance Applicative CheckM where
+  pure _ = CheckM $ \_ -> Just "Variable not in scope: "
+  (<*>) = ap
 
--- instance Monad CheckM where
---   CheckM m >>= f = CheckM $ \env ->
---     let (err, x) = m env
---     in case err of
---          Just e  -> (Just e, undefined) -- The second part is not used
---          Nothing -> let (err', x') = runCheckM (f x) env
---                     in (err', x')
+instance Monad CheckM where 
+  return = pure 
+  (>>=) (CheckM a) f = CheckM $ \exp ->
+    case a exp of 
+      Just x -> pure x
+      Nothing -> 
 
--- -- Helper function to check if a variable is in scope
--- checkVarInScope :: VName -> CheckM ()
--- checkVarInScope var = CheckM $ \env ->
---   if var `elem` env
---     then (Nothing, ())
---     else (Just $ "Variable not in scope: " ++ var, ())
 
--- -- Helper function to extend scope
--- extendScope :: VName -> CheckM a -> CheckM a
--- extendScope var (CheckM m) = CheckM $ \env -> m (var : env)
 
--- -- Run the CheckM monad
--- runCheckM :: CheckM a -> [VName] -> (Maybe Error, a)
--- runCheckM (CheckM m) = m
+checkExp :: Exp -> Maybe Error 
+checkExp = undefined
 
--- checkExp :: Exp -> Maybe Error
--- checkExp exp = case runCheckM (check exp) [] of
---   (Just err, _) -> Just err
---   (Nothing, _)  -> Nothing
-
--- check :: Exp -> CheckM ()
--- check (CstInt _) = return ()
--- check (CstBool _) = return ()
--- check (Var v) = checkVarInScope v
--- check (Add e1 e2) = check e1 >> check e2
--- check (Sub e1 e2) = check e1 >> check e2
--- check (Mul e1 e2) = check e1 >> check e2
--- check (Div e1 e2) = check e1 >> check e2
--- check (Pow e1 e2) = check e1 >> check e2
--- check (Eql e1 e2) = check e1 >> check e2
--- check (If cond e1 e2) = check cond >> check e1 >> check e2
--- check (Let var e1 e2) = do
---   check e1
---   extendScope var $ check e2
--- check (Lambda var body) = extendScope var $ check body
--- check (Apply e1 e2) = check e1 >> check e2
--- check (TryCatch e1 e2) = check e1 >> check e2
--- check (Print _ e1) = check e1
--- check (KvPut e1 e2) = check e1 >> check e2
--- check (KvGet e1) = check e1
--- week3/handout/src/APL/Parser.hs
+++ week3/handout/src/APL/Parser.hs
@@ -13,20 +13,113 @@ import Text.Megaparsec
     many,
     notFollowedBy,
     parse,
-    parseTest,
     satisfy,
     some,
     try,
   )
 import Text.Megaparsec.Char (space)
 
--- Do not change this definition.
 type Parser = Parsec Void String
 
-pExp :: Parser Exp
-pExp = undefined
+lexeme :: Parser a -> Parser a
+lexeme p = p <* space
 
--- Do not change this definition.
+keywords :: [String]
+keywords =
+  [ "if",
+    "then",
+    "else",
+    "true",
+    "false",
+    "let",
+    "in",
+    "try",
+    "catch",
+    "print",
+    "put",
+    "get"
+  ]
+
+lVName :: Parser VName
+lVName = lexeme $ try $ do
+  c <- satisfy isAlpha
+  cs <- many $ satisfy isAlphaNum
+  let v = c : cs
+  if v `elem` keywords
+    then fail "Unexpected keyword"
+    else pure v
+
+lInteger :: Parser Integer
+lInteger =
+  lexeme $ read <$> some (satisfy isDigit) <* notFollowedBy (satisfy isAlphaNum)
+
+lString :: String -> Parser ()
+lString s = lexeme $ void $ chunk s
+
+lKeyword :: String -> Parser ()
+lKeyword s = lexeme $ void $ try $ chunk s <* notFollowedBy (satisfy isAlphaNum)
+
+lBool :: Parser Bool
+lBool =
+  lexeme . try . choice $
+    [ const True <$> lKeyword "true",
+      const False <$> lKeyword "false"
+    ]
+
+pAtom :: Parser Exp
+pAtom =
+  choice
+    [ CstInt <$> lInteger,
+      CstBool <$> lBool,
+      Var <$> lVName,
+      lString "(" *> pExp <* lString ")"
+    ]
+
+pLExp :: Parser Exp
+pLExp =
+  choice
+    [ If
+        <$> (lKeyword "if" *> pExp0)
+        <*> (lKeyword "then" *> pExp0)
+        <*> (lKeyword "else" *> pExp0),
+      pAtom
+    ]
+
+pExp1 :: Parser Exp
+pExp1 = pLExp >>= chain
+  where
+    chain x =
+      choice
+        [ do
+            lString "*"
+            y <- pLExp
+            chain $ Mul x y,
+          do
+            lString "/"
+            y <- pLExp
+            chain $ Div x y,
+          pure x
+        ]
+
+pExp0 :: Parser Exp
+pExp0 = pExp1 >>= chain
+  where
+    chain x =
+      choice
+        [ do
+            lString "+"
+            y <- pExp1
+            chain $ Add x y,
+          do
+            lString "-"
+            y <- pExp1
+            chain $ Sub x y,
+          pure x
+        ]
+
+pExp :: Parser Exp
+pExp = pExp0
+
 parseAPL :: FilePath -> String -> Either String Exp
 parseAPL fname s = case parse (space *> pExp <* eof) fname s of
   Left err -> Left $ errorBundlePretty err
